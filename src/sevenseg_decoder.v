0f7560dc359c677782d427b030ddd5c96b208b7e
stevooooooo <github noreply stimperley85@gmail.com@users.noreply.github.com>
2025-02-21 22:24:57 -0700

    update

---
+++ src/sevenseg_decoder.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/21/2025 10:21:17 PM
+// Design Name: 
+// Module Name: sevenseg_decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sevenseg_decoder(
+    input [3:0] i_Hex,
+    output [6:0] o_seg_n
+    );
+endmodule
